366782887f3c2fa60dbd6b5bbb6ce0dd28b3f1b9
dacare2425 <davidcalpar@gmail.com>
2025-04-18 13:45:30 +0200

    Eliminando archivos no correspondientes al pj

--- src/main/java/edu/fje/daw2/sm72/repositoris/ClientRepositori.java
+++
@@ -1,8 +0,0 @@
-package edu.fje.daw2.sm72.repositoris;
-
-import edu.fje.daw2.sm72.models.Client;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientRepositori extends CrudRepository<Client, Long> {
-
-}
