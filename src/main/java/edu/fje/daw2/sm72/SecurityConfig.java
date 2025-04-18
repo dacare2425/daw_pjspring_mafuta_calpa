3bfe097788db344945b253ca4e6cee6793b4e0e6
dacare2425 <davidcalpar@gmail.com>
2025-04-18 16:48:51 +0200

    Oauth funcional

---
+++ src/main/java/edu/fje/daw2/sm72/SecurityConfig.java
@@ -0,0 +1,20 @@
+package edu.fje.daw2.sm72;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders
+        .HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+        return http.build();
+    }
+}
